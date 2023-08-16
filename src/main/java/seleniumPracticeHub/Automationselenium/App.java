04d993d4c668a86d4bd769d095734881c8cdda9b
anvesh <anvesh1122>
2023-08-16 23:51:14 +0530

    2023

---
+++ src/main/java/seleniumPracticeHub/Automationselenium/App.java
@@ -0,0 +1,13 @@
+package seleniumPracticeHub.Automationselenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
